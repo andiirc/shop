6e5b4ae76f56d110cf99565d0be6a16946f2826a
anderson.rodriguez <anderson.rodriguez@guane.com.co>
2023-03-25 16:53:44 -0500

    :fire: remove style no use

--- src/ui/screens/ShoppingCart.js
+++ src/ui/screens/ShoppingCart.js
@@ -19,15 +19,3 @@ export default function ShoppingCart() {
     </Fragment>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop:20,
-  },  
-
-  scrollView: {
-    backgroundColor: '#fff',
-  },
-
-});
